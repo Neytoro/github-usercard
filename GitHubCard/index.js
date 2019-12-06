513993423a2cafd647f447826272e4e221016401
Neytorokx <neytorokx@gmail.com>
2019-12-05 18:23:10 -0600

    Initial Push

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,85 @@
            https://api.github.com/users/<your name>
 */
 
+const users = [ 'Neytoro', 'tetondan',
+'dustinmyers',
+'justsml',
+'luishrd',
+'bigknell' ];
+
+function buildCard(data) {
+
+  console.log(data.data.name);
+
+  const card = document.createElement('div');
+  const cardInfo = document.createElement('div');
+
+  const profilePic = document.createElement('img');
+
+  const header = document.createElement('h3');
+
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  const link = document.createElement('a');
+
+  { // Class assignments
+
+    card.className = 'card';
+    cardInfo.className = 'card-info';
+
+    header.className = 'name';
+
+    username.className = 'username';
+
+  }
+
+  { // Data assignments
+
+    profilePic.src = data.data.avatar_url;
+
+    header.innerHTML = data.data.name;
+
+    username.innerHTML = data.data.login;
+    location.innerHTML = data.data.location;
+    followers.innerHTML = data.data.followers;
+    following.innerHTML = data.data.following;
+    bio.innerHTML = data.data.bio;
+
+    link.innerHTML = data.data.html_url;
+    link.href = data.data.html_url;
+
+  }
+
+  { // Appending
+
+    profile.appendChild(link);
+
+    cardInfo.appendChild(header);
+    cardInfo.appendChild(username);
+    cardInfo.appendChild(location);
+    cardInfo.appendChild(profile);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(bio);
+  
+    card.appendChild(profilePic);
+    card.appendChild(cardInfo);
+  
+  }
+
+  return card;
+
+}
+
+users.forEach(data => axios.get('https://api.github.com/users/' + data).then((response) => { document.querySelector('.cards').appendChild(buildCard(response)); }).catch((error) => console.log(error)).finally(() => {}))
+
+// axios.get('https://api.github.com/users/Neytoro').then((response) => { neytorokx = response; console.log(response); }).catch((error) => console.log(error)).finally(() => {});
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -23,8 +102,6 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
